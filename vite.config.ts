96368b53df0ac2d3210e3ac8b5f5edff2d0ef3e8
SiHyun <there7788@gmail.com>
2025-04-21 18:38:43 +0900

    Chore: vite-tsconfig-paths 플러그인 추가
    - Vite 환경에서 빌드 시 alias를 설정하기 위해 플러그인 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   server: {
     host: true,
     port: 3000,
